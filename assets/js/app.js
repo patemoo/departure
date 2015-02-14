9e94f802ffc59d8ccca09c99271841770b12388c
patemoo <patemoo@comcast.net>
2015-02-14 13:54:26 -0800

    basic angular setup

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('DepartureApp', ['ui.bootstrap','ngRoute']);
+
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
+
+  $locationProvider.html5Mode(true);
+
+  $routeProvider
+  .when('/',{
+    templateUrl: '/views/home.html',
+    controller: 'HomeCtrl'
+  });
+
+}]);
